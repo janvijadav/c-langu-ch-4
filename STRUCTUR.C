92c818ade5ac98913266f6b8fdec33892b4d7e64
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-10-10 16:17:14 +0530

    Add files via upload

---
+++ STRUCTUR.C
@@ -0,0 +1,20 @@
+
+	///structure example 2
+	//example of initialization of stru.
+
+	struct Demo
+	{
+		int a;
+		float b;
+		char c;
+	};
+	void main()
+	{
+		struct Demo d={100,19.234,'c'};
+		clrscr();
+		printf("\n a=%d",d.a);
+		printf("\n b=%.2f",d.b);
+		printf("\n c=%c",d.c);
+		printf("\n struct Demo occupy %d bytes",sizeof(d));
+		getch();
+	}
